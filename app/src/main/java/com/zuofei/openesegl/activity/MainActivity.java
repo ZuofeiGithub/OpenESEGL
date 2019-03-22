e59c9c12b9abd62533bff195f72dc9a1b4630b56
zuofei <348068347@qq.com>
2019-03-22 16:41:58 +0800

    更新APP

--- app/src/main/java/com/zuofei/openesegl/activity/MainActivity.java
+++ app/src/main/java/com/zuofei/openesegl/activity/MainActivity.java
@@ -1,22 +1,48 @@
 package com.zuofei.openesegl.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import rx.functions.Action1;
 
 import android.opengl.GLES20;
 import android.os.Bundle;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
+import android.widget.Toast;
 
+import com.tbruyelle.rxpermissions.RxPermissions;
+import com.vector.update_app.UpdateAppManager;
+import com.vector.update_app.listener.ExceptionHandler;
 import com.zuofei.openesegl.utils.EglHelper;
 import com.zuofei.openesegl.R;
+import com.zuofei.openesegl.utils.UpdateAppHttpUtil;
+
+import static android.Manifest.permission.WRITE_EXTERNAL_STORAGE;
 
 public class MainActivity extends AppCompatActivity {
     private SurfaceView surfaceView;
+    private String mUpdateUrl = "https://raw.githubusercontent.com/WVector/AppUpdateDemo/master/json/json.txt";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         surfaceView = findViewById(R.id.surfaceview);
+        getPermission();
+        new UpdateAppManager
+                .Builder()
+                //当前Activity
+                .setActivity(this)
+                //更新地址
+                .setUpdateUrl(mUpdateUrl)
+                .handleException(new ExceptionHandler() {
+                    @Override
+                    public void onException(Exception e) {
+                        e.printStackTrace();
+                    }
+                })
+                //实现httpManager接口的对象
+                .setHttpManager(new UpdateAppHttpUtil())
+                .build()
+                .update();
         surfaceView.getHolder().addCallback(new SurfaceHolder.Callback() {
             @Override
             public void surfaceCreated(SurfaceHolder holder) {
@@ -40,6 +66,22 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+    }
+
+    public void getPermission() {
+        RxPermissions rxPermissions = new RxPermissions(this);
+        rxPermissions.request(WRITE_EXTERNAL_STORAGE)
+                .subscribe(new Action1<Boolean>() {
+                    @Override
+                    public void call(Boolean aBoolean) {
+                        if (aBoolean) {
+                            Toast.makeText(MainActivity.this, "已授权", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(MainActivity.this, "未授权", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
     }
 
     private void render(SurfaceHolder holder, int width, int height) {
